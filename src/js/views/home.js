3b482137818356ca396f48d46516b7e5fd4b41a9
Vanessa <126210620+nessarz93@users.noreply.github.com>
2023-10-07 02:27:33 +0000

    Agregar card

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,24 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Aquí debe ir la lista de contactos</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div classNameName="text-center mt-5">
+		
+		<div className="card mb-3">
+			<div className="row g-0">
+				<div className="col-md-2">
+					<img Link="..." className="img-fluid rounded-start" alt="..."/>
+				</div>
+				<div className="col-md-10">
+					<div className="card-body">
+						<h3 className="card-title">Nombre</h3>
+						<h6 className="card-text text-secondary">Dirección</h6>
+						<p className="card-text text-secondary">Teléfono</p>
+						<p className="card-text text-secondary">Correo</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
 	</div>
 );
